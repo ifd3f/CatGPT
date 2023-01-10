10cfb3588e22c3cb26ecb3cc46b0a1411e65ddb9
Astrid Yu <astrid@astrid.tech>
2023-01-10 13:56:50 -0800

    Make it nya in cyrillic

--- catgpt.py
+++ catgpt.py
@@ -95,24 +95,13 @@ def generate_meow():
     return 'meow' * randint(1, 6)
 
 
-def generate_chinese():
-    return '喵' * randint(1, 6)
-
-
-def generate_japanese():
-    return 'ニャン' * randint(1, 3)
-
-
-def generate_spanish():
-    return 'ña' * randint(1, 6)
-
-
 def pick_generator():
-    if randint(1, 10) == 1:
+    if randint(1, 15) == 1:
         return choice([
-            generate_spanish,
-            generate_chinese,
-            generate_japanese,
+            lambda: "ня" * randint(1, 6),
+            lambda: "ニャン" * randint(1, 6),
+            lambda: "喵" * randint(1, 6),
+            lambda: "ña" * randint(1, 6)
         ])
 
     return choice([
@@ -125,7 +114,7 @@ def pick_generator():
 def generate_any():
     return ' '.join([
         pick_generator()()
-        for i in range(randint(1, 10))
+        for i in range(randint(1, 8))
     ])
 
 
